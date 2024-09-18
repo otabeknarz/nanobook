54c6ea3eef4e4ec41dd7546e488f09fd370bd753
Otabek Narziqulov <otabeknarz@gmail.com>
2024-09-19 04:24:10 +0500

    created CustomUser model

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
-from django.db import models
 from .functions import get_random_id, get_random_promocode
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class BaseModel(models.Model):
@@ -10,7 +12,7 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class Promocode(BaseModel):
+class PromoCode(BaseModel):
     class Discounts(models.IntegerChoices):
         D_10 = 10, "10%"
         D_20 = 20, "20%"
@@ -39,11 +41,11 @@ class Pay(BaseModel):
         editable=True,
         max_length=12,
     )
-    # user = models.OneToOneField(CustomUser, related_name="pays")
+    user = models.OneToOneField("CustomUser", related_name="pays", on_delete=models.CASCADE)
     value = models.CharField(max_length=32)
     is_done_successfully = models.BooleanField(default=False)
-    promocode = models.OneToOneField(
-        Promocode, related_name="pay", null=True, blank=True, on_delete=models.SET_NULL
+    promo_code = models.OneToOneField(
+        PromoCode, related_name="pay", null=True, blank=True, on_delete=models.SET_NULL
     )
 
     def __str__(self):
@@ -72,6 +74,48 @@ class Book(BaseModel):
     )
     name = models.CharField(max_length=255)
     slug = models.CharField(max_length=255, blank=True)
-    # authors = models.ManyToManyField(CustomUser, related_name="books")
+    authors = models.ManyToManyField("CustomUser", related_name="books")
     categories = models.ManyToManyField(Category, related_name="books")
     file = models.FileField(upload_to=user_directory_path)
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, phone_number=None, password=None, **extra_fields):
+        if not username:
+            raise ValueError(_('The Username field must be set'))
+        if not phone_number:
+            raise ValueError(_('The Phone Number field must be set'))
+
+        user = self.model(username=username, phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+
+        return self.create_user(username, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(_('username'), max_length=150, unique=True)
+    phone_number = models.CharField(_('phone number'), max_length=15, unique=True, blank=True, null=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    is_active = models.BooleanField(_('active'), default=True)
+    is_staff = models.BooleanField(_('staff status'), default=False)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+
+    def __str__(self):
+        return self.username
