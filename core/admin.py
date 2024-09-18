a08bd24b0a235c306fcfb322361aa850ee3369c7
Otabek Narziqulov <otabeknarz@gmail.com>
2024-09-19 04:25:26 +0500

    added CustomUserAdmin class

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'phone_number', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+
+    readonly_fields = ('date_joined', 'last_login')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'phone_number')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'phone_number', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
+    )
+
+    search_fields = ('username', 'phone_number')
+    ordering = ('username',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
