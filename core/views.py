596622209a1005082b18f22b79b859dacfd36360
Otabek Narziqulov <otabeknarz@gmail.com>
2024-09-19 05:02:30 +0500

    implemented login and logout functions

--- core/views.py
+++ core/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import logout, login, authenticate
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def index(request):
+    if request.user.is_authenticated:
+        return render(request, "main.html")
+
+    return render(request, "index.html")
+
+
+def logout_page(request):
+    logout(request)
+    return redirect("core:index")
+
+
+def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("core:index")
+        else:
+            return render(request, "page2.html", {"error": "Parol yoki login xato"})
+    return render(request, "page2.html")
