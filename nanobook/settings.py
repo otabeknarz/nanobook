c649dd7a69d1929ae6a49927bcbd872cfd4d9d95
Otabek Narziqulov <otabeknarz@gmail.com>
2024-09-19 05:02:00 +0500

    settings for static files and AUTH_USER_MODEL

--- nanobook/settings.py
+++ nanobook/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = "nanobook.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,11 +117,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = "core.CustomUser"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
